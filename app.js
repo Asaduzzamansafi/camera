7aa27cd111b5e23802330c8f715545feaec4ba7d
Asaduzzamansafi <asaduzzamansafi@gmail.com>
2022-07-28 02:02:31 +0600

    done

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+let VIDEO=null;
+let CANVAS=null;
+let CONTEXT=null;
+
 function main(){
+    CANVAS=document.getElementById("mycanvas");
+    CONTEXT=CANVAS.getContext("2d");
+    CANVAS.width=window.innerWidth;
+    CANVAS.height=window.innerHeight;
     let promise = navigator.mediaDevices.getUserMedia({video:true});
+    promise.then(function(sinnal){
+        VIDEO=document.createElement("video");
+        VIDEO.srcObject=sinnal;
+        VIDEO.play();
+
+        VIDEO.onloadeddata=function(){
+            updatecanvas();
+        }
+
+    }).catch(function(err){
+        alert("camera error: "+err);
+    });
+}
+
+function updatecanvas(){
+    CONTEXT.drawImage(VIDEO,0,0);
+    window.requestAnimationFrame(updatecanvas);
 }
