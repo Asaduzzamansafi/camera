50a4e4add3978f7755482c2267da87ba26fc7ab2
Asaduzzamansafi <asaduzzamansafi@gmail.com>
2022-07-28 21:04:44 +0600

    done last

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
-let VIDEO=null;
-let CANVAS=null;
-let CONTEXT=null;
+// let VIDEO=null;
+// let CANVAS=null;
+// let CONTEXT=null;
 
-function main(){
-    CANVAS=document.getElementById("mycanvas");
-    CONTEXT=CANVAS.getContext("2d");
-    CANVAS.width=window.innerWidth;
-    CANVAS.height=window.innerHeight;
-    let promise = navigator.mediaDevices.getUserMedia({video:true});
-    promise.then(function(sinnal){
-        VIDEO=document.createElement("video");
-        VIDEO.srcObject=sinnal;
-        VIDEO.play();
+// function main(){
+//     CANVAS=document.getElementById("mycanvas");
+//     CONTEXT=CANVAS.getContext("2d");
+//     CANVAS.width=window.innerWidth;
+//     CANVAS.height=window.innerHeight;
+//     let promise = navigator.mediaDevices.getUserMedia({video:true});
+//     promise.then(function(sinnal){
+//         VIDEO=document.createElement("video");
+//         VIDEO.srcObject=sinnal;
+//         VIDEO.play();
 
-        VIDEO.onloadeddata=function(){
-            updatecanvas();
-        }
+//         VIDEO.onloadeddata=function(){
+//             updatecanvas();
+//         }
 
-    }).catch(function(err){
-        alert("camera error: "+err);
-    });
+//     }).catch(function(err){
+//         alert("camera error: "+err);
+//     });
+// }
+
+// function updatecanvas(){
+//     CONTEXT.drawImage(VIDEO,0,0);
+//     window.requestAnimationFrame(updatecanvas);
+// }
+
+let canvas =document.getElementById("canvas");
+let context = canvas.getContext("2d");
+let video = document.getElementById("video");
+
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+    navigator.mediaDevices.getUserMedia({ video: true}).then((stream)=>{
+        video.srcObject = stream;
+        video.play();
+    })
 }
-
-function updatecanvas(){
-    CONTEXT.drawImage(VIDEO,0,0);
-    window.requestAnimationFrame(updatecanvas);
-}
+document.getElementById("snap").addEventListener('click', ()=>{
+    context.drawImage(video,0,0,640,480);
+})
